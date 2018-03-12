51258ed1562e3e7463c8262eee18403d0b4a08c5
LAPTOP-G5FTFL6V\Ponycorn <magnus@floweducation.io>
2018-03-12 20:47:11 +0000

    derp

---
+++ server.js
@@ -0,0 +1,10 @@
+
+//Install express server
+const express = require('express');
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// Start the app by listening on 0he default Heroku port
+app.listen(4200);
